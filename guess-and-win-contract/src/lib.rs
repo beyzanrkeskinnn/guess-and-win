04f77c26546565468761c157e958c97394e6e722
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2025-06-04 08:59:23 -0700

    contract sections are being created

--- guess-and-win-contract/src/lib.rs
+++ guess-and-win-contract/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![no_std]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod admin;
+mod allowance;
+mod balance;
+mod contract;
+mod metadata;
+mod storage_types;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use crate::contract::TokenClient;
